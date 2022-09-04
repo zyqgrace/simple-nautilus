08bec2c69a79f0919f580692477973ca01115be1
yangqing zheng <yzhe6302@uni.sydney.edy.au>
2022-09-04 12:24:47 +1000

    test1

--- nautilus.py
+++ nautilus.py
@@ -1,6 +1,30 @@
+class Tree():
+    def __init__(self,root):
+        self.root = root
+        self.child = []
+
+    def __str__(self):
+        return f'root:/$ {self.root}'
+
+    def addNode(self,child):
+        self.child.append(child)
+
+class Node():
+    def __init__(self,data):
+        self.data = data
+        self.child = []
+    def addNode(self,obj):
+        self.child.append(obj)
+
 def main():
     # TODO
-    pass
+    command = input()
+    pwd = Tree("/")
+    while command != "exit":
+        if command == "pwd":
+            print(pwd)
+        command = input()
+    print(f"root:/$ bye, {pwd.root}")
 
 
 if __name__ == '__main__':
