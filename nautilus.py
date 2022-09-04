a7bfa46b2da37b3c7a08a5ecbedd4a49ec863a57
yangqing zheng <yzhe6302@uni.sydney.edy.au>
2022-09-04 14:51:46 +1000

    test2

--- nautilus.py
+++ nautilus.py
@@ -1,30 +1,60 @@
 class Tree():
-    def __init__(self,root):
-        self.root = root
+    def __init__(self):
+        self.user = "root"
+        self.parent = None
+        self.pwd = self
         self.child = []
 
     def __str__(self):
-        return f'root:/$ {self.root}'
+        return f'{self.user}:/$ '
 
     def addNode(self,child):
         self.child.append(child)
+    
+    def absolutepath(self):
+        return "/"
 
 class Node():
-    def __init__(self,data):
-        self.data = data
-        self.child = []
-    def addNode(self,obj):
-        self.child.append(obj)
+    def __init__(self,filename,parent):
+        self.user = "root"
+        self.filename = filename
+        self.parent = parent
+        self.children = []
+
+    def __str__(self):
+        return f'{self.user}:{self.absolutepath()}$ '
+
+    def absolutepath(self):
+        if self.parent != "/":
+            return self.parent.absolutepath() + "/" + self.pwd
+        return "/" + self.pwd
+    
+    
+
+def check_absolute(path,root):
+    if root.absolutepath()==path:
+        return root
+    if len(root.child) > 0:
+        for child in root.child:
+            check_absolute(path,child)
 
 def main():
     # TODO
-    command = input()
-    pwd = Tree("/")
-    while command != "exit":
-        if command == "pwd":
-            print(pwd)
-        command = input()
-    print(f"root:/$ bye, {pwd.root}")
+    root = Tree()
+
+    command = (input(root.pwd)).split(" ")
+    
+    while command[0] != "exit":
+        if command[0] == "pwd":
+            print(root.absolutepath())
+            
+        elif command[0] == 'cd':
+            if command[1][0]=="/":
+                if check_absolute(command[1],root):
+                    root.pwd = check_absolute(command[1],root)
+
+        command = (input(root)).split(" ")
+    print(f"bye, {root.user}")
 
 
 if __name__ == '__main__':
